54c8445f68eb89329e1343bb241479e2b11aa25b
JIYA-R <86499018+JIYA-R@users.noreply.github.com>
2021-11-08 17:38:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function Login(){
+    number1=document.getElementById("Number1").value;
+    number2=document.getElementById("Number2").value;
+    actual_answer=parseInt(number1) * parseInt(number2);
+    Question= "<h4>" + Number1 + "X"+ Number2 + "</h4>";
+}
